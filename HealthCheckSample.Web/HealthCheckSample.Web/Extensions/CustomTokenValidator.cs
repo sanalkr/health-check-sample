2471915b8dc6ca7120d825d9e8edb94c827f44d4
sanalkr <sanals@live.in>
2022-12-30 12:37:00 +0530

    Authorization check

---
+++ HealthCheckSample.Web/HealthCheckSample.Web/Extensions/CustomTokenValidator.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace HealthCheckSample.Web.Extensions
+{
+    public class CustomTokenValidator : JwtSecurityTokenHandler
+    {
+
+        public override ClaimsPrincipal ValidateToken(string token, TokenValidationParameters validationParameters, out SecurityToken validatedToken)
+        {
+            try
+            {
+
+                var claimsPrincipal = base.ValidateToken(token, validationParameters, out validatedToken);
+
+                return claimsPrincipal;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        protected override JwtSecurityToken ValidateSignature(string token, TokenValidationParameters validationParameters)
+        {
+            var jwt = new JwtSecurityToken(token);
+
+            return jwt;
+        }
+    }
+}
--- HealthCheckSample.Web/HealthCheckSample.Web/Program.cs
+++ HealthCheckSample.Web/HealthCheckSample.Web/Program.cs
@@ -1,7 +1,12 @@
 
 
+using HealthCheckSample.Web.Extensions;
 using HealthCheckSample.Web.HealthChecks;
 using HealthCheckSample.Web.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +15,22 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.Configure<ConnectionStrings>(builder.Configuration.GetSection("ConnectionStrings"));
 builder.Services.Configure<ApiSettingOptions>(builder.Configuration.GetSection(ApiSettingOptions.Name));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(ops => {
+        ops.Audience = "myapp";
+        //ops.Authority = "myissuer";
+        ops.RequireHttpsMetadata = false;
+        ops.SecurityTokenValidators.Clear();
+        ops.SecurityTokenValidators.Add(new CustomTokenValidator());
+        ops.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidIssuer = "myissuer",
+            ValidAudience = "myapp",
+            RequireSignedTokens = false,
+            IssuerSigningKey = new SymmetricSecurityKey(new HMACSHA512(Encoding.UTF8.GetBytes("mysecretkey")).Key) { KeyId = "123" }
+        };
+    });
 
 builder.Services.AddHealthChecks()
     .AddCheck<ApplicationHealthCheck>("ApplicationHealthCheck")
@@ -36,6 +57,9 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapHealthChecks("/health");
+
+app.MapHealthChecks("/health/all")
+    .RequireHost("*:7061")
+    .RequireAuthorization();
 
 app.Run();
