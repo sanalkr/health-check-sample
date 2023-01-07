6e6e46657e6d2af2a1714023822117d5890c1e68
sanalkr <sanals@live.in>
2023-01-07 15:02:00 +0530

    Update changes for Authentication, filters and publisher

---
+++ HealthCheckSample.Web/HealthCheckSample.Web/HealthChecks/CustomHealthCheckPublisher.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthCheckSample.Web.HealthChecks
+{
+    public class CustomHealthCheckPublisher : IHealthCheckPublisher
+    {
+        public Task PublishAsync(HealthReport report, CancellationToken cancellationToken)
+        {
+            switch (report.Status)
+            {
+                case HealthStatus.Unhealthy:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case HealthStatus.Degraded:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case HealthStatus.Healthy:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine($"{DateTime.UtcNow} Prob status: {report.Status}.");
+
+            Console.ResetColor();
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- HealthCheckSample.Web/HealthCheckSample.Web/Program.cs
+++ HealthCheckSample.Web/HealthCheckSample.Web/Program.cs
@@ -2,6 +2,13 @@
 
 using HealthCheckSample.Web.HealthChecks;
 using HealthCheckSample.Web.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,9 +19,34 @@ builder.Services.Configure<ConnectionStrings>(builder.Configuration.GetSection("
 builder.Services.Configure<ApiSettingOptions>(builder.Configuration.GetSection(ApiSettingOptions.Name));
 
 builder.Services.AddHealthChecks()
-    .AddCheck<ApplicationHealthCheck>("ApplicationHealthCheck")
+    .AddCheck<ApplicationHealthCheck>("ApplicationHealthCheck", tags: new string[] { "simple" })
     .AddSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-    
+
+builder.Services.Configure<HealthCheckPublisherOptions>(ops =>
+{
+    ops.Period = TimeSpan.FromSeconds(5);
+    ops.Delay = TimeSpan.FromSeconds(10);
+    ops.Predicate = hc => hc.Tags.Contains("simple");
+});
+
+builder.Services.AddSingleton<IHealthCheckPublisher, CustomHealthCheckPublisher>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(ops =>
+    {
+        ops.Audience = "myapp";
+        ops.RequireHttpsMetadata = false;
+        ops.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidIssuer = "myissuer",
+            ValidAudience = "myapp",
+            SignatureValidator = delegate (string token, TokenValidationParameters validationParameters)
+            {
+                var jwt = new JwtSecurityToken(token);
+                return jwt;
+            }
+        };
+    });
 
 var app = builder.Build();
 
@@ -29,6 +61,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseAuthentication();
+
 app.UseRouting();
 
 app.UseAuthorization();
@@ -36,6 +70,64 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapHealthChecks("/health");
+
+app.MapHealthChecks("/health/all", new HealthCheckOptions
+{
+    ResultStatusCodes = {
+        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+        [HealthStatus.Degraded] = StatusCodes.Status200OK,
+        [HealthStatus.Unhealthy] = StatusCodes.Status500InternalServerError
+    }
+})
+    .RequireHost("*:7061");
+    //.RequireAuthorization();
+
+app.MapHealthChecks("/hc", new HealthCheckOptions
+{
+    Predicate = hc => hc.Tags.Contains("simple")
+});
+
+app.MapHealthChecks("/hcdetail", new HealthCheckOptions
+{
+    ResponseWriter = WriteResponse
+});
+
+Task WriteResponse(HttpContext context, HealthReport healthReport)
+{
+    context.Response.ContentType = "application/json; charset=utf-8";
+
+    var options = new JsonWriterOptions { Indented = true };
+
+    using var memoryStream = new MemoryStream();
+    using (var jsonWriter = new Utf8JsonWriter(memoryStream, options))
+    {
+        jsonWriter.WriteStartObject();
+        jsonWriter.WriteString("status", healthReport.Status.ToString());
+        jsonWriter.WriteStartObject("results");
+
+        foreach (var entry in healthReport.Entries)
+        {
+            jsonWriter.WriteStartObject(entry.Key);
+            jsonWriter.WriteString("status", entry.Value.Status.ToString());
+            jsonWriter.WriteString("description", entry.Value.Description);
+            jsonWriter.WriteStartObject("data");
+
+            foreach (var item in entry.Value.Data)
+            {
+                jsonWriter.WritePropertyName(item.Key);
+                JsonSerializer.Serialize(jsonWriter, item.Value,
+                    item.Value?.GetType() ?? typeof(object));
+            }
+
+            jsonWriter.WriteEndObject();
+            jsonWriter.WriteEndObject();
+        }
+
+        jsonWriter.WriteEndObject();
+        jsonWriter.WriteEndObject();
+    }
+
+    return context.Response.WriteAsync(Encoding.UTF8.GetString(memoryStream.ToArray()));
+}
 
 app.Run();
